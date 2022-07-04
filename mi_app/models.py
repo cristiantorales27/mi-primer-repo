b89759a6aac28b6076c82d288ad8fd4295fc07ea
Cristian Torales <cristiantorales@hotmail.com>
2022-07-03 21:28:39 -0300

    mi primer mvt

--- mi_app/models.py
+++ mi_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+class Curso(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+
 # Create your models here.
--- mi_app/views.py
+++ mi_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date, datetime
+from mi_app.models import Curso
 # Create your views here.
 
 def saludo(request):
@@ -11,3 +12,19 @@ def saludo(request):
 def saludar_a(request,nombre):
     return HttpResponse(f"Hola como estas {nombre}")
 
+
+def saludo_personalizado(request):
+    context = {}
+
+    if request.GET:
+            context["nombre"] = request.GET["nombre"]
+
+    return render(request,"mi_app/index.html",context)
+
+
+def listar_cursos(request):
+    context = {}
+
+    context["cursos"] = Curso.objects.all()
+
+    return render(request, "mi_app/lista_cursos.html",context)
