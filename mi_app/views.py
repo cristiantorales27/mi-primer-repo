a3fa5affcb60b9af86193ef9451e6f527c0677e5
Cristian Torales <cristiantorales@hotmail.com>
2022-07-18 21:01:43 -0300

    Arreglo formulario de busqueda

--- mi_app/views.py
+++ mi_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from datetime import date, datetime
 from mi_app.models import Curso, Estudiantes
 from mi_app.models import Familia
+from mi_app.forms import CursoFormulario, CursoBusquedaFormulario
 # Create your views here.
 
 def saludo(request):
@@ -39,3 +40,37 @@ def listar_estudiantes(request):
 
     context["estudiantes"] = Estudiantes.objects.all()
     return render(request, "mi_app/lista_estudiantes.html",context)
+
+def formulario_curso(request):
+
+    if request.method == "POST":
+
+        mi_formulario = CursoFormulario(request.POST)
+
+        if  mi_formulario.is_valid:
+            datos = mi_formulario.cleaned_data 
+            curso = Curso(nombre=datos["nombre"], camada=datos["camada"])
+            curso.save()
+
+            return render(request, "mi_app/curso_formulario.html", {"mensaje":"agregado con exito!"})
+
+    else:
+
+        mi_formulario = CursoFormulario()
+
+    return render(request, "mi_app/curso_formulario.html", {"mi_formulario":mi_formulario})
+
+
+
+def formulario_busqueda(request):
+
+    busqueda_formulario = CursoBusquedaFormulario()
+
+
+    if request.GET:      
+        cursos = Curso.objects.filter(nombre=busqueda_formulario["criterio"]).all()
+        return render(request, "mi_app/curso_busqueda.html", {"busqueda_formulario": busqueda_formulario, "cursos": cursos})
+
+
+    return render(request, "mi_app/curso_busqueda.html", {"busqueda_formulario": busqueda_formulario})
+
